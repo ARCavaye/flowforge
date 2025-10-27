86e63501568cb54467098ce44f022254066e41c2
Alasdair Cavaye <16743887+ARCavaye@users.noreply.github.com>
2025-10-27 16:18:02 +0000

    Add fields to venue model: phone, email, and risk assessment URL

--- src/core/models.py
+++ src/core/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class venue(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    riskAssessmentUrl = models.URLField()
 
     def __str__(self):
         return self.name
